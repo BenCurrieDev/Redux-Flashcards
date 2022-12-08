aae370bb18cf98edb5c7c3f19b10112612049611
Ben Currie <69808500+BJCurrie@users.noreply.github.com>
2022-12-08 18:16:33 +0000

    created cards slice

---
+++ src/features/cards/cardsSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = { cards: {} }
+
+const cardsSlice = createSlice({
+    name: 'cards',
+    initialState,
+    reducers: {
+        addCard(state, action) {
+            state.cards[action.payload.id] = action.payload;
+        }
+    }
+})
+
